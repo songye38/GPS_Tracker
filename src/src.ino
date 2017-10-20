cb7b90b8bc720a657c5fbedbe88bdb417d461b16
songye38 <vsongyev@hanmail.net>
2017-10-20 15:23:28 +0900

    remove nokia5110 module and add rgb led 8bit

--- src/src.ino
+++ src/src.ino
@@ -1,52 +1,39 @@
+#include <Adafruit_NeoPixel.h>
 #include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
+#include <SPI.h>
+#include <SD.h>
 
 
-// Test code for Adafruit GPS modules using MTK3329/MTK3339 driver
-//
-// This code shows how to listen to the GPS module in an interrupt
-// which allows the program to have more 'freedom' - just parse
-// when a new NMEA sentence is available! Then access data when
-// desired.
-//
-// Tested and works great with the Adafruit Ultimate GPS module
-// using MTK33x9 chipset
-//    ------> http://www.adafruit.com/products/746
-// Pick one up today at the Adafruit electronics shop 
-// and help support open source hardware & software! -ada
 
-#define RST 10 //10번 핀에 RST를 연결합니다.
-#define CE  9 //9번 핀에 CE를 연결합니다.
-#define DC  8 //8번 핀에 DC를 연결합니다.
-#define DIN  7 //7번 핀에 DIN을 연결합니다.
-#define CLK  6 //6번 핀에 CLK를 연결합니다.
+/*
+ * setting for rgb led 8 bit
+ */
+ const int PIN = 7;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, PIN, NEO_GRB + NEO_KHZ800);
+const int onoff =0;
+const int gps = 1;
+const int sd = 2;
+const int tracking =3;
+const int battery = 4;
 
-// If you're using a GPS module:
-// Connect the GPS Power pin to 5V
-// Connect the GPS Ground pin to ground
-// If using software serial (sketch example default):
-//   Connect the GPS TX (transmit) pin to Digital 3
-//   Connect the GPS RX (receive) pin to Digital 2
-// If using hardware serial (e.g. Arduino Mega):
-//   Connect the GPS TX (transmit) pin to Arduino RX1, RX2 or RX3
-//   Connect the GPS RX (receive) pin to matching TX1, TX2 or TX3
+ /*
+  * setting for sd card
+  */
+File myFile;
+const int chipSelect = 4;
 
-// If you're using the Adafruit GPS shield, change 
-// SoftwareSerial mySerial(3, 2); -> SoftwareSerial mySerial(8, 7);
-// and make sure the switch is set to SoftSerial
-
-// If using software serial, keep this line enabled
-// (you can change the pin numbers to match your wiring):
+/*
+ * setting for gps module
+ */
 SoftwareSerial mySerial(3, 2);
+Adafruit_GPS GPS(&mySerial);
 
 // If using hardware serial (e.g. Arduino Mega), comment out the
 // above SoftwareSerial line, and enable this line instead
 // (you can change the Serial number to match your wiring):
 
 //HardwareSerial mySerial = Serial1;
-
-
-Adafruit_GPS GPS(&mySerial);
 
 
 // Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
@@ -60,72 +47,72 @@ void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
 
 static const byte ASCII[][5] = // 아스키코드의 문자열을 나열합니다.
 {
- {0x00, 0x00, 0x00, 0x00, 0x00} // 20  
-,{0x00, 0x00, 0x5f, 0x00, 0x00} // 21 !
-,{0x00, 0x07, 0x00, 0x07, 0x00} // 22 "
-,{0x14, 0x7f, 0x14, 0x7f, 0x14} // 23 #
-,{0x24, 0x2a, 0x7f, 0x2a, 0x12} // 24 $
-,{0x23, 0x13, 0x08, 0x64, 0x62} // 25 %
-,{0x36, 0x49, 0x55, 0x22, 0x50} // 26 &
-,{0x00, 0x05, 0x03, 0x00, 0x00} // 27 '
-,{0x00, 0x1c, 0x22, 0x41, 0x00} // 28 (
-,{0x00, 0x41, 0x22, 0x1c, 0x00} // 29 )
-,{0x14, 0x08, 0x3e, 0x08, 0x14} // 2a *
-,{0x08, 0x08, 0x3e, 0x08, 0x08} // 2b +
-,{0x00, 0x50, 0x30, 0x00, 0x00} // 2c ,
-,{0x08, 0x08, 0x08, 0x08, 0x08} // 2d -
-,{0x00, 0x60, 0x60, 0x00, 0x00} // 2e .
-,{0x20, 0x10, 0x08, 0x04, 0x02} // 2f /
-,{0x3e, 0x51, 0x49, 0x45, 0x3e} // 30 0
-,{0x00, 0x42, 0x7f, 0x40, 0x00} // 31 1
-,{0x42, 0x61, 0x51, 0x49, 0x46} // 32 2
-,{0x21, 0x41, 0x45, 0x4b, 0x31} // 33 3
-,{0x18, 0x14, 0x12, 0x7f, 0x10} // 34 4
-,{0x27, 0x45, 0x45, 0x45, 0x39} // 35 5
-,{0x3c, 0x4a, 0x49, 0x49, 0x30} // 36 6
-,{0x01, 0x71, 0x09, 0x05, 0x03} // 37 7
-,{0x36, 0x49, 0x49, 0x49, 0x36} // 38 8
-,{0x06, 0x49, 0x49, 0x29, 0x1e} // 39 9
-,{0x00, 0x36, 0x36, 0x00, 0x00} // 3a :
-,{0x00, 0x56, 0x36, 0x00, 0x00} // 3b ;
-,{0x08, 0x14, 0x22, 0x41, 0x00} // 3c <
-,{0x14, 0x14, 0x14, 0x14, 0x14} // 3d =
-,{0x00, 0x41, 0x22, 0x14, 0x08} // 3e >
-,{0x02, 0x01, 0x51, 0x09, 0x06} // 3f ?
-,{0x32, 0x49, 0x79, 0x41, 0x3e} // 40 @
-,{0x7e, 0x11, 0x11, 0x11, 0x7e} // 41 A
-,{0x7f, 0x49, 0x49, 0x49, 0x36} // 42 B
-,{0x3e, 0x41, 0x41, 0x41, 0x22} // 43 C
-,{0x7f, 0x41, 0x41, 0x22, 0x1c} // 44 D
-,{0x7f, 0x49, 0x49, 0x49, 0x41} // 45 E
-,{0x7f, 0x09, 0x09, 0x09, 0x01} // 46 F
-,{0x3e, 0x41, 0x49, 0x49, 0x7a} // 47 G
-,{0x7f, 0x08, 0x08, 0x08, 0x7f} // 48 H
-,{0x00, 0x41, 0x7f, 0x41, 0x00} // 49 I
-,{0x20, 0x40, 0x41, 0x3f, 0x01} // 4a J
-,{0x7f, 0x08, 0x14, 0x22, 0x41} // 4b K
-,{0x7f, 0x40, 0x40, 0x40, 0x40} // 4c L
-,{0x7f, 0x02, 0x0c, 0x02, 0x7f} // 4d M
-,{0x7f, 0x04, 0x08, 0x10, 0x7f} // 4e N
-,{0x3e, 0x41, 0x41, 0x41, 0x3e} // 4f O
-,{0x7f, 0x09, 0x09, 0x09, 0x06} // 50 P
-,{0x3e, 0x41, 0x51, 0x21, 0x5e} // 51 Q
-,{0x7f, 0x09, 0x19, 0x29, 0x46} // 52 R
-,{0x46, 0x49, 0x49, 0x49, 0x31} // 53 S
-,{0x01, 0x01, 0x7f, 0x01, 0x01} // 54 T
-,{0x3f, 0x40, 0x40, 0x40, 0x3f} // 55 U
-,{0x1f, 0x20, 0x40, 0x20, 0x1f} // 56 V
-,{0x3f, 0x40, 0x38, 0x40, 0x3f} // 57 W
-,{0x63, 0x14, 0x08, 0x14, 0x63} // 58 X
-,{0x07, 0x08, 0x70, 0x08, 0x07} // 59 Y
-,{0x61, 0x51, 0x49, 0x45, 0x43} // 5a Z
-,{0x00, 0x7f, 0x41, 0x41, 0x00} // 5b [
-,{0x02, 0x04, 0x08, 0x10, 0x20} // 5c ¥
-,{0x00, 0x41, 0x41, 0x7f, 0x00} // 5d ]
-,{0x04, 0x02, 0x01, 0x02, 0x04} // 5e ^
-,{0x40, 0x40, 0x40, 0x40, 0x40} // 5f _
-,{0x00, 0x01, 0x02, 0x04, 0x00} // 60 `
-,{0x20, 0x54, 0x54, 0x54, 0x78} // 61 a
+// {0x00, 0x00, 0x00, 0x00, 0x00} // 20  
+//,{0x00, 0x00, 0x5f, 0x00, 0x00} // 21 !
+//,{0x00, 0x07, 0x00, 0x07, 0x00} // 22 "
+//,{0x14, 0x7f, 0x14, 0x7f, 0x14} // 23 #
+//,{0x24, 0x2a, 0x7f, 0x2a, 0x12} // 24 $
+//,{0x23, 0x13, 0x08, 0x64, 0x62} // 25 %
+//,{0x36, 0x49, 0x55, 0x22, 0x50} // 26 &
+//,{0x00, 0x05, 0x03, 0x00, 0x00} // 27 '
+//,{0x00, 0x1c, 0x22, 0x41, 0x00} // 28 (
+//,{0x00, 0x41, 0x22, 0x1c, 0x00} // 29 )
+//,{0x14, 0x08, 0x3e, 0x08, 0x14} // 2a *
+//,{0x08, 0x08, 0x3e, 0x08, 0x08} // 2b +
+//,{0x00, 0x50, 0x30, 0x00, 0x00} // 2c ,
+//,{0x08, 0x08, 0x08, 0x08, 0x08} // 2d -
+//,{0x00, 0x60, 0x60, 0x00, 0x00} // 2e .
+//,{0x20, 0x10, 0x08, 0x04, 0x02} // 2f /
+// {0x3e, 0x51, 0x49, 0x45, 0x3e} // 30 0
+//,{0x00, 0x42, 0x7f, 0x40, 0x00} // 31 1
+//,{0x42, 0x61, 0x51, 0x49, 0x46} // 32 2
+//,{0x21, 0x41, 0x45, 0x4b, 0x31} // 33 3
+//,{0x18, 0x14, 0x12, 0x7f, 0x10} // 34 4
+//,{0x27, 0x45, 0x45, 0x45, 0x39} // 35 5
+//,{0x3c, 0x4a, 0x49, 0x49, 0x30} // 36 6
+//,{0x01, 0x71, 0x09, 0x05, 0x03} // 37 7
+//,{0x36, 0x49, 0x49, 0x49, 0x36} // 38 8
+//,{0x06, 0x49, 0x49, 0x29, 0x1e} // 39 9
+//,{0x00, 0x36, 0x36, 0x00, 0x00} // 3a :
+//,{0x00, 0x56, 0x36, 0x00, 0x00} // 3b ;
+//,{0x08, 0x14, 0x22, 0x41, 0x00} // 3c <
+//,{0x14, 0x14, 0x14, 0x14, 0x14} // 3d =
+//,{0x00, 0x41, 0x22, 0x14, 0x08} // 3e >
+//,{0x02, 0x01, 0x51, 0x09, 0x06} // 3f ?
+//,{0x32, 0x49, 0x79, 0x41, 0x3e} // 40 @
+//,{0x7e, 0x11, 0x11, 0x11, 0x7e} // 41 A
+//,{0x7f, 0x49, 0x49, 0x49, 0x36} // 42 B
+//,{0x3e, 0x41, 0x41, 0x41, 0x22} // 43 C
+//,{0x7f, 0x41, 0x41, 0x22, 0x1c} // 44 D
+//,{0x7f, 0x49, 0x49, 0x49, 0x41} // 45 E
+//,{0x7f, 0x09, 0x09, 0x09, 0x01} // 46 F
+//,{0x3e, 0x41, 0x49, 0x49, 0x7a} // 47 G
+//,{0x7f, 0x08, 0x08, 0x08, 0x7f} // 48 H
+//,{0x00, 0x41, 0x7f, 0x41, 0x00} // 49 I
+//,{0x20, 0x40, 0x41, 0x3f, 0x01} // 4a J
+//,{0x7f, 0x08, 0x14, 0x22, 0x41} // 4b K
+//,{0x7f, 0x40, 0x40, 0x40, 0x40} // 4c L
+//,{0x7f, 0x02, 0x0c, 0x02, 0x7f} // 4d M
+//,{0x7f, 0x04, 0x08, 0x10, 0x7f} // 4e N
+//,{0x3e, 0x41, 0x41, 0x41, 0x3e} // 4f O
+//,{0x7f, 0x09, 0x09, 0x09, 0x06} // 50 P
+//,{0x3e, 0x41, 0x51, 0x21, 0x5e} // 51 Q
+//,{0x7f, 0x09, 0x19, 0x29, 0x46} // 52 R
+//,{0x46, 0x49, 0x49, 0x49, 0x31} // 53 S
+//,{0x01, 0x01, 0x7f, 0x01, 0x01} // 54 T
+//,{0x3f, 0x40, 0x40, 0x40, 0x3f} // 55 U
+//,{0x1f, 0x20, 0x40, 0x20, 0x1f} // 56 V
+//,{0x3f, 0x40, 0x38, 0x40, 0x3f} // 57 W
+//,{0x63, 0x14, 0x08, 0x14, 0x63} // 58 X
+//,{0x07, 0x08, 0x70, 0x08, 0x07} // 59 Y
+//,{0x61, 0x51, 0x49, 0x45, 0x43} // 5a Z
+//,{0x00, 0x7f, 0x41, 0x41, 0x00} // 5b [
+//,{0x02, 0x04, 0x08, 0x10, 0x20} // 5c ¥
+//,{0x00, 0x41, 0x41, 0x7f, 0x00} // 5d ]
+//,{0x04, 0x02, 0x01, 0x02, 0x04} // 5e ^
+//,{0x40, 0x40, 0x40, 0x40, 0x40} // 5f _
+//,{0x00, 0x01, 0x02, 0x04, 0x00} // 60 `
+ {0x20, 0x54, 0x54, 0x54, 0x78} // 61 a
 ,{0x7f, 0x48, 0x44, 0x44, 0x38} // 62 b
 ,{0x38, 0x44, 0x44, 0x44, 0x20} // 63 c
 ,{0x38, 0x44, 0x44, 0x48, 0x7f} // 64 d
@@ -158,97 +145,26 @@ static const byte ASCII[][5] = // 아스키코드의 문자열을 나열합니�
 ,{0x78, 0x46, 0x41, 0x46, 0x78} // 7f →
 };
 
-void LcdWriteString(char *characters)
-{ // String값을 LCD에 출력하는 함수
-  while(*characters) LcdWriteCharacter(*characters++);
-}
 
-void LcdWriteCharacter(char character)
-{ // char값을 LCD에 출력하는 함수
-  for(int i=0; i<5; i++) LcdWriteData(ASCII[character - 0x20][i]);
-  LcdWriteData(0x00);
-}
-
-void LcdWriteData(byte dat)
-{ // byte값을 LCD에 출력하는 함수
-  digitalWrite(DC, HIGH); 
-  digitalWrite(CE, LOW);
-  shiftOut(DIN, CLK, MSBFIRST, dat); // 시리얼 데이터를 LCD에 전송합니다.
-  digitalWrite(CE, HIGH);
-}
-
-void LcdXY(int x, int y)
-{ // LCD의 X, Y좌표값을 설정해 주는 함수
-  LcdWriteCmd(0x80 | x);  // 커서의 가로위치를 지정합니다.
-  LcdWriteCmd(0x40 | y);  // 커서의 세로위치를 지정합니다.
-}
-
-void LcdWriteCmd(byte cmd)
-{
-  digitalWrite(DC, LOW); //커맨드 입력을 위해 LOW로 설정합니다.
-  digitalWrite(CE, LOW);
-  shiftOut(DIN, CLK, MSBFIRST, cmd); //시리얼 데이터를 LCD에 전송합니다.
-
-  digitalWrite(CE, HIGH);
-}
 
 void setup()  
 {
-  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
-  // also spit it out
+
   Serial.begin(115200);
   Serial.println("Adafruit GPS library basic test!");
-
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
   GPS.begin(9600);
-  
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+ 
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
-  
-  // Set the update rate
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
-
-  // Request updates on antenna status, comment out to keep quiet
   GPS.sendCommand(PGCMD_ANTENNA);
-
-  // the nice thing about this code is you can have a timer0 interrupt go off
-  // every 1 millisecond, and read data from the GPS for you. that makes the
-  // loop code a heck of a lot easier!
   useInterrupt(true);
-
-  pinMode(RST, OUTPUT);
-  pinMode(CE, OUTPUT);
-  pinMode(DC, OUTPUT);
-  pinMode(DIN, OUTPUT);
-  pinMode(CLK, OUTPUT);
-  digitalWrite(RST, LOW);
-  digitalWrite(RST, HIGH);
-  // LCD와 연결된 각 핀을 설정합니다.
-
-  
-  LcdWriteCmd(0x21);  // LCD 확장 명령
-  LcdWriteCmd(0xBF);  // LCD의 콘트라스트(명암을 결정합니다)
-  LcdWriteCmd(0x04);  
-  LcdWriteCmd(0x14);  
-  LcdWriteCmd(0x20);  
-  LcdWriteCmd(0x0C);  
-
-  for(int i=0; i<504; i++) LcdWriteData(0x00); //LCD를 초기화 합니다
-  
-  LcdXY(20,0);
-  LcdWriteString("KOCOA FAB");
-  // 시작시 화면 가운데에 KOCOA FAB 글자를 띄워줍니다.  
-
-  delay(1000);
-  // Ask for firmware version
-  
   mySerial.println(PMTK_Q_RELEASE);
+
+  strip.begin();
+  strip.show(); 
+  initialize_sd();
+  set_green_pin(0);
+  
 }
 
 
@@ -279,8 +195,57 @@ void useInterrupt(boolean v) {
   }
 }
 
+
+void loop()                     
+{
+  read_gps_data();
+}
+
+void initialize_sd()
+{
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    set_red_pin(2);
+    return;
+  }
+  Serial.println("initialization done.");
+  set_green_pin(2);
+}
+
+/*
+ * code for rgb led 8bit strip
+ */
+
+void set_red_pin(uint16_t pinNum)
+{
+  uint32_t c = strip.Color(255,0,0);
+  strip.setPixelColor(pinNum,c);
+  strip.show();
+}
+void set_blue_pin(uint16_t pinNum)
+{
+  uint32_t c = strip.Color(0,0,255);
+  strip.setPixelColor(pinNum,c);
+  strip.show();
+}
+void set_green_pin(uint16_t pinNum)
+{
+  uint32_t c = strip.Color(0,255,0);
+  strip.setPixelColor(pinNum,c);
+  strip.show();
+}
+void set_yellow_pin(uint16_t pinNum)
+{
+  uint32_t c = strip.Color(255,255,0);
+  strip.setPixelColor(pinNum,c);
+  strip.show();
+}
+
+
 uint32_t timer = millis();
-void loop()                     // run over and over again
+void read_gps_data()
 {
   // in case you are not using the interrupt above, you'll
   // need to 'hand query' the GPS, not suggested :(
@@ -321,11 +286,7 @@ void loop()                     // run over and over again
     char *fTime = new char[time.length() + 1];
     strcpy(fTime, time.c_str());
     // 시분초를 HH:MM:SS형태로 바꿔주고, String값을 char*로 바꿔주어 LCD에 출력할 수 있도록 합니다.
-    
-    //for(int i=0; i<504; i++) LcdWriteData(0x00); //LCD를 초기화 합니다
-    
-    LcdXY(0,2);
-    LcdWriteString(fTime);
+   
     delete [] fTime;
     // 시분초를 LCD에 출력해 줍니다.
     
@@ -338,14 +299,12 @@ void loop()                     // run over and over again
     strcpy(fDate, date.c_str());
     // 년월일를 YY/MM/DD형태로 바꿔주고, String값을 char*로 바꿔주어 LCD에 출력할 수 있도록 합니다.
 
-    
-    LcdXY(0,1);
-    LcdWriteString(fDate);
     delete [] fDate;
     // 년월일을 LCD에 출력해 줍니다.
     
     
     if (GPS.fix) {
+      set_green_pin(1);
       char lat[20];
       char loc[20];
       dtostrf(GPS.latitude, 9, 4, lat);
@@ -365,14 +324,15 @@ void loop()                     // run over and over again
       // GPS.latitude와 GPS.longitude값을 받아오면 소숫점 자리가 4번째 자리에 찍혀 있습니다.
       // latitude = 3728,8640, longitude = 12700.8960
       // 이것을 소수점 두번째 자리에 찍는 작업입니다.    
-      LcdXY(0,3);
-      LcdWriteString(lat);
-      Serial.print("lat : ");
-      Serial.println(lat);
-      LcdXY(0,4);
-      LcdWriteString(loc);
+    
       // latitude값과 longitude값을 LCD에 출력합니다.
     }
+    else {
+      set_blue_pin(1);
+    }
   }
+  
 }
+
+
 
